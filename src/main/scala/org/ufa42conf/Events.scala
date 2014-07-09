6673a9cd30aa47f8b1f8360342a1e9ec10b0a3b4
Lev Khomich <levkhomich@gmail.com>
2014-07-09 17:06:30 +0600

    update talks

--- src/main/scala/org/ufa42conf/Events.scala
+++ src/main/scala/org/ufa42conf/Events.scala
@@ -70,13 +70,13 @@ object ConfEvents {
         "",
         User(9600972L, "levkhomich", "Lev Khomich", Some("https://pbs.twimg.com/profile_images/459340276188708864/b3X4WwoB.png"))
       ),
-      Talk("Распределенные event-driven системы",
+      Talk("5 Event-driven лайфхаков для вашего кода",
         "",
         User(16135676L, "abdullin", "Rinat Abdullin", Some("http://pbs.twimg.com/profile_images/3479036762/40c99d96aa9a4e57cfa7d54d1fb7d5b2.jpeg"))
       )
     ),
     List(
-      Talk("Docker - пакуем приложения",
+      Talk("Мобильное приложение для управления презентацией за 30 минут",
         "",
         User(16135676L, "abdullin", "Rinat Abdullin", Some("http://pbs.twimg.com/profile_images/3479036762/40c99d96aa9a4e57cfa7d54d1fb7d5b2.jpeg"))
       ),
